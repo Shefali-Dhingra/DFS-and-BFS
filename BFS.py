f81408f0d9ca9177befd30116c5cc4d9e7312ce8
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2021-03-16 12:05:30 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,23 +1,25 @@
-from time import time
+from time import time  #to display time in seconds
 graph = {
-  'Homepage' : ['Director','Movie'],
-  'Director': ['Name','Description'],
-  'Name': [],
+  'Homepage' : ['Director','Movie'], #level 0- Homepage, level 1- Director and Movie (child nodes)
+  'Director': ['Name','Description'],  #level 2- Name and Description
+  'Name': [], 
   'Description': [],
-  'Movie' : ['Categories'],
-  'Categories' : ['SciFi','Comedy'],
-  'SciFi' : ['Transformers','Iron Man'],
+  'Movie' : ['Categories'], #level 2
+  'Categories' : ['SciFi','Comedy'], #level 3- Scifi and Comedy
+  'SciFi' : ['Transformers','Iron Man'], #level 4- Transformers and Iron Man (Child node for Scifi)
   'Comedy' : ['Dictator'],
   'Transformers':[],
   'Iron Man':[],
   'Dictator':[]
 }
-visited = [] 
-queue = []     #Initialise
+#Elements on same level are neighbours
+visited = []  #To track the list of visited nodes
+queue = []     #Initialising a queue
+               #BFS uses queue 
 
 def bfs(visited, graph, node):
-  visited.append(node)
-  queue.append(node)
+  visited.append(node) #Tracking each visited node
+  queue.append(node) #Tracking BFS for the graph
   while queue:
     s = queue.pop(0) 
     print (s, end = "\n") 
@@ -27,6 +29,6 @@ def bfs(visited, graph, node):
         queue.append(neighbour)
 
 t0 = time()
-bfs(visited, graph, 'Homepage')
+bfs(visited, graph, 'Homepage') #Can change the 'node' value to check the time for that node
 t1 = time() - t0
 print('Time for BFS :', t1, 'seconds')
